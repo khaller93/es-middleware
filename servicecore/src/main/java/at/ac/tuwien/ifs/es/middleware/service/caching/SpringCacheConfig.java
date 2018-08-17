1a503df97033bd7324ce0b0d685abbfd79098219
Kevin Haller <kevin.haller@outofbits.com>
2018-08-17 13:42:57 +0200

    #1: Adds new caching config.

---
+++ servicecore/src/main/java/at/ac/tuwien/ifs/es/middleware/service/caching/SpringCacheConfig.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.ifs.es.middleware.service.caching;
+
+import java.util.Arrays;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableCaching
+public class SpringCacheConfig {
+
+  @Bean
+  public CacheManager cacheManager() {
+    SimpleCacheManager cacheManager = new SimpleCacheManager();
+    cacheManager.setCaches(Arrays.asList(
+        new ConcurrentMapCache("sparql"),
+        new ConcurrentMapCache("fts"),
+        new ConcurrentMapCache("sameas"),
+        new ConcurrentMapCache("lcs"),
+        new ConcurrentMapCache("similarity")));
+    return cacheManager;
+  }
+
+}
