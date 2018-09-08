9da5bf6860f9739ca03458377c4b32388fa633eb
Kevin Haller <kevin.haller@outofbits.com>
2018-09-08 08:37:58 +0200

    #5. Adds instant verification of given arguments for dynamic exploration flow requests (fail fast).

--- dto/src/main/java/at/ac/tuwien/ifs/es/middleware/dto/exploration/request/DynamicExplorationFlowRequest.java
+++ dto/src/main/java/at/ac/tuwien/ifs/es/middleware/dto/exploration/request/DynamicExplorationFlowRequest.java
@@ -1,11 +1,18 @@
 package at.ac.tuwien.ifs.es.middleware.dto.exploration.request;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 /**
+ * This class represents a dynamic exploration flow, which consists of a number of steps executing a
+ * registered operator.
  *
+ * @author Kevin Haller
+ * @version 1.0
+ * @since 1.0
  */
 public class DynamicExplorationFlowRequest {
 
@@ -14,6 +21,8 @@ public class DynamicExplorationFlowRequest {
   @JsonCreator
   public DynamicExplorationFlowRequest(
       @JsonProperty(value = "steps", required = true) List<ExplorationFlowStepRequest> steps) {
+    checkArgument(steps != null && !steps.isEmpty(),
+        "A flow must have one or more steps specified.");
     this.steps = steps;
   }
 
--- dto/src/main/java/at/ac/tuwien/ifs/es/middleware/dto/exploration/request/ExplorationFlowStepRequest.java
+++ dto/src/main/java/at/ac/tuwien/ifs/es/middleware/dto/exploration/request/ExplorationFlowStepRequest.java
@@ -1,11 +1,21 @@
 package at.ac.tuwien.ifs.es.middleware.dto.exploration.request;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * This class represents one step in a exploration flow. A step must have a name referencing to a
+ * registered operator and optionally specifies arguments for the execution of the operator.
+ *
+ * @author Kevin Haller
+ * @version 1.0
+ * @since 1.0
+ */
 public class ExplorationFlowStepRequest {
 
   private String name;
@@ -14,6 +24,8 @@ public class ExplorationFlowStepRequest {
   @JsonCreator
   public ExplorationFlowStepRequest(@JsonProperty(value = "name", required = true) String name,
       @JsonProperty(value = "param") ObjectNode param) {
+    checkArgument(name != null && !name.isEmpty(),
+        "The name of the step must be specified and not be empty.");
     this.name = name;
     this.param = param == null || param.isNull() ? JsonNodeFactory.instance.objectNode() : param;
   }
